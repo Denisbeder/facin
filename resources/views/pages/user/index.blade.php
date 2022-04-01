5538dce79792bc462536f110821b2b138e9016ce
denisbeder <denisbeder@gmail.com>
2022-04-01 08:10:30 -0400

    use component flex list to test

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -10,5 +10,29 @@
         </x-slot>
     </x-topbar>
 
-    <livewire:counter />
+    <div class="p-6">
+        <x-flex-list>
+            <x-flex-list.header>
+                <x-flex-list.cell>Nome</x-flex-list.cell>
+                <x-flex-list.cell>E-mail</x-flex-list.cell>
+                <x-flex-list.cell>Estado</x-flex-list.cell>
+            </x-flex-list.header>
+
+            @for($i = 0; $i < 20; $i++)
+            <x-flex-list.row>
+                <x-flex-list.cell header="Nome">
+                    Denisbeder
+                </x-flex-list.cell>
+
+                <x-flex-list.cell header="E-mail">
+                    denisbeder@gmail.com
+                </x-flex-list.cell>
+
+                <x-flex-list.cell header="Estado">
+                    Ativado
+                </x-flex-list.cell>
+            </x-flex-list.row>
+            @endfor
+        </x-flex-list>
+    </div>
 @endsection
