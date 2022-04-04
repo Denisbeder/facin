39d530310b7ccfeba028d48076395781b88d72af
denisbeder <denisbeder@gmail.com>
2022-04-04 11:00:48 -0400

    add position align

--- resources/views/components/dropdown/index.blade.php
+++ resources/views/components/dropdown/index.blade.php
@@ -1,15 +1,15 @@
-@props(['open' => false])
+@props(['open' => false, 'aligny' => 'bottom', 'alignx' => 'left', 'variant' => null])
 
 <div {{ $attributes->merge(['class' => 'relative inline-flex']) }} 
     x-data="{dropdownOpen: {{ $open ? 'true' : 'false' }} }" 
     x-on:click.outside="dropdownOpen = false" 
-    x-on:keydown.escape.window="dropdownOpen = false"
->
+    x-on:keydown.escape.window="dropdownOpen = false">
+
     <div x-on:click="dropdownOpen = !dropdownOpen" class="cursor-pointer">
         @isset($trigger)
             {{ $trigger }}
         @else
-            <x-button variant="white">
+            <x-button variant="{{ $variant }}">
                 <x-icon name="dots-vertical-rounded" size="text-base" />
             </x-button>
         @endisset
@@ -17,7 +17,7 @@
     
     <ul 
         :class="{'!opacity-100 !pointer-events-auto': dropdownOpen}" 
-        class="mt-2 drop-shadow-xl absolute top-full z-10 bg-white border rounded-lg pointer-events-none transition-all opacity-0 duration-300 w-36"
+        class="drop-shadow-xl absolute {{ $alignx }}-0 {{ $aligny == 'bottom' ? 'mt-2  top-full' : '-top-2 -translate-y-full' }} z-10 bg-white border rounded-lg pointer-events-none transition-all opacity-0 duration-300 w-36"
     >
         {{ $slot }}
     </ul>
