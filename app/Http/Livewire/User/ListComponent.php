e72c39f736ee55d4e4694705fb298deabe3f20b9
denisbeder <denisbeder.carvalho@devsquad.com>
2022-10-24 23:58:57 -0400

    add action for delete

--- app/Http/Livewire/User/ListComponent.php
+++ app/Http/Livewire/User/ListComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Actions\UserDelete;
+use App\Actions\UserDeleteAll;
 use App\Models\User;
 use App\Traits\WithBulkActions;
 use App\Traits\WithPerPagePagination;
@@ -11,21 +13,36 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\Component;
 
 /**
- * @property mixed $dataQuery
- * @property mixed $data
+ * @property Builder $query
+ * @property LengthAwarePaginator $data
  */
 class ListComponent extends Component
 {
     use WithPerPagePagination, WithBulkActions;
 
-    public function getDataQueryProperty(): Builder
+    public function getQueryProperty(): Builder
     {
         return User::query();
     }
 
     public function getDataProperty(): LengthAwarePaginator
     {
-        return $this->applyPagination($this->dataQuery->latest('id'));
+        return $this->applyPagination($this->query->latest('id'));
+    }
+
+    public function deleteSelectedKeys(): void
+    {
+        if (empty($this->selectedKeys)) {
+            return;
+        }
+
+        if ($this->selectAll) {
+            UserDeleteAll::run($this->selectAllExcept);
+        } else {
+            UserDelete::run($this->selectedKeys);
+        }
+
+        $this->clearAllSelected();
     }
 
     public function render(): View
