03410f6cc2a0bdcdc7841c1a4b025afeeaf4a74b
denisbeder <denisbeder.carvalho@devsquad.com>
2022-12-30 15:04:14 -0400

    added radio select

--- resources/views/pages-form.blade.php
+++ resources/views/pages-form.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <form>
-        <x-container class="max-w-2xl mx-auto">
+        <x-container class="max-w-4xl mx-auto">
             <x-page-heading title="Criar página">
                 <x-slot:actions class="justify-between">
                     <x-button href="/" color="white" leftIcon="arrow-small-left">Cancelar</x-button>
@@ -27,6 +27,92 @@
                             <div class="mt-1 w-full">
                                 <x-input.text id="slug" name="slug" type="text" autocomplete="off"/>
                             </div>
+                        </div>
+
+                        <div class="col-auto">
+                            @php
+                                $pageTypes = [
+                                    [
+                                        'value' => 'default',
+                                        'label' => 'Padrão',
+                                        'description' => 'Página única',
+                                        'icon' => 'document-text',
+                                    ],
+                                    [
+                                        'value' => 'post',
+                                        'label' => 'Post',
+                                        'description' => 'Postagens em geral',
+                                        'icon' => 'newspaper',
+                                    ],
+                                    [
+                                        'value' => 'form',
+                                        'label' => 'Formulário',
+                                        'description' => 'Página com formulário',
+                                        'icon' => 'envelope',
+                                    ],
+                                    [
+                                        'value' => 'promotion',
+                                        'label' => 'Promoção',
+                                        'description' => 'Sorteio de prêmios',
+                                        'icon' => 'gift',
+                                    ],
+                                    [
+                                        'value' => 'ad',
+                                        'label' => 'Anúncio',
+                                        'description' => 'Mural de anúncios',
+                                        'icon' => 'megaphone',
+                                    ],
+                                    [
+                                        'value' => 'shop',
+                                        'label' => 'Loja',
+                                        'description' => 'Venda de Info-Produtos',
+                                        'icon' => 'shopping-bag',
+                                    ],
+                                ];
+                            @endphp
+                            <fieldset x-data="{
+                                selected: null,
+                            }">
+                                <legend class="block text-sm font-medium text-gray-700">Selecione o tipo de página</legend>
+
+                                <div class="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
+                                    @foreach($pageTypes as $type)
+                                    <label
+                                        class="relative select-none flex flex-col cursor-pointer rounded-lg border border-gray-200 bg-white p-4 focus:outline-none"
+                                        :class="selected === '{{ $type['value'] }}' && 'bg-indigo-50/30'"
+                                        @click="selected = '{{ $type['value'] }}'"
+                                    >
+                                        <input
+                                            type="radio"
+                                            name="type"
+                                            value="{{ $type['value'] }}"
+                                            class="sr-only"
+                                            aria-labelledby="page-type-0-label"
+                                            aria-describedby="page-type-0-description-0"
+                                        >
+
+                                        <span class="text-gray-400" :class="selected === '{{ $type['value'] }}' && 'text-indigo-500'">
+                                            <x-dynamic-component component="icon.{{ $type['icon'] }}" class="h-8 w-8" />
+                                        </span>
+
+                                        <span class="flex flex-1 mt-3">
+                                            <span class="flex flex-col">
+                                              <span id="page-type-0-label" class="block text-sm font-semibold text-gray-900" :class="selected === '{{ $type['value'] }}' && 'text-indigo-500'">{{ $type['label'] }}</span>
+                                              <span id="page-type-0-description-0" class="mt-1 flex items-center text-sm text-gray-500">{{ $type['description'] }}</span>
+                                            </span>
+
+                                            <x-icon.check-circle class="h-5 w-5 text-indigo-600 absolute top-4 right-4" ::class="selected !== '{{ $type['value'] }}' && 'invisible'" />
+                                        </span>
+
+                                        <span
+                                            class="pointer-events-none absolute -inset-px rounded-md"
+                                            :class="selected === '{{ $type['value'] }}' && 'border-2 border-indigo-500'"
+                                            aria-hidden="true"
+                                        ></span>
+                                    </label>
+                                    @endforeach
+                                </div>
+                            </fieldset>
                         </div>
 
                         <div class="col-auto">
